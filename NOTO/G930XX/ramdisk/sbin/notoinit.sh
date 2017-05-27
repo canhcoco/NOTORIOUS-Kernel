b13cad95fdbb0f65bf173a3076bef2caeccbb4df
FAROVITUS <farovitus@gmail.com>
2017-05-26 21:56:05 -0400

    ramdisk: Add MTweaks support + NOTO'S TOUCH on notoinit

--- NOTO/G930XX/ramdisk/sbin/notoinit.sh
+++ NOTO/G930XX/ramdisk/sbin/notoinit.sh
@@ -23,11 +23,84 @@ else
 fi;
 fi;
 
+MTWEAKS_PATH=/data/.mtweaks
+
 # Mount
 $BB mount -t rootfs -o remount,rw rootfs;
 $BB mount -o remount,rw /system;
 $BB mount -o remount,rw /data;
 $BB mount -o remount,rw /;
+
+# Set I/O Scheduler tweaks mmcblk0
+	chmod 644 /sys/block/mmcblk0/queue/scheduler
+	echo maple > /sys/block/mmcblk0/queue/scheduler
+	echo 2048 > /sys/block/mmcblk0/queue/read_ahead_kb
+  chmod 644 /sys/block/mmcblk0/queue/iosched/writes_starved
+	echo 4 > /sys/block/mmcblk0/queue/iosched/writes_starved
+  chmod 644 /sys/block/mmcblk0/queue/iosched/fifo_batch
+	echo 16 > /sys/block/mmcblk0/queue/iosched/fifo_batch
+	echo 350 > /sys/block/mmcblk0/queue/iosched/sync_read_expire
+	echo 550 > /sys/block/mmcblk0/queue/iosched/sync_write_expire
+	echo 250 > /sys/block/mmcblk0/queue/iosched/async_read_expire
+	echo 450 > /sys/block/mmcblk0/queue/iosched/async_write_expire
+	echo 10 > /sys/block/mmcblk0/queue/iosched/sleep_latency_multiple
+
+# Set I/O Scheduler tweaks sda
+  chmod 644 /sys/block/sda/queue/scheduler
+	echo maple > /sys/block/sda/queue/scheduler
+	echo 512 > /sys/block/sda/queue/read_ahead_kb
+	echo 4 > /sys/block/sda/queue/iosched/writes_starved
+	echo 16 > /sys/block/sda/queue/iosched/fifo_batch
+	echo 350 > /sys/block/sda/queue/iosched/sync_read_expire
+	echo 550 > /sys/block/sda/queue/iosched/sync_write_expire
+	echo 250 > /sys/block/sda/queue/iosched/async_read_expire
+	echo 450 > /sys/block/sda/queue/iosched/async_write_expire
+	echo 10 > /sys/block/sda/queue/iosched/sleep_latency_multiple
+
+# Set I/O Scheduler tweaks sdb
+	chmod 644 /sys/block/sdb/queue/scheduler
+  echo maple > /sys/block/sdb/queue/scheduler
+  echo 512 > /sys/block/sdb/queue/read_ahead_kb
+  echo 4 > /sys/block/sdb/queue/iosched/writes_starved
+	echo 16 > /sys/block/sdb/queue/iosched/fifo_batch
+	echo 350 > /sys/block/sdb/queue/iosched/sync_read_expire
+	echo 550 > /sys/block/sdb/queue/iosched/sync_write_expire
+	echo 250 > /sys/block/sdb/queue/iosched/async_read_expire
+	echo 450 > /sys/block/sdb/queue/iosched/async_write_expire
+	echo 10 > /sys/block/sdb/queue/iosched/sleep_latency_multiple
+
+# Set I/O Scheduler tweaks sdc
+	chmod 644 /sys/block/sdc/queue/scheduler
+	echo maple > /sys/block/sdc/queue/scheduler
+	echo 512 > /sys/block/sdc/queue/read_ahead_kb
+	echo 4 > /sys/block/sdc/queue/iosched/writes_starved
+	echo 16 > /sys/block/sdc/queue/iosched/fifo_batch
+	echo 350 > /sys/block/sdc/queue/iosched/sync_read_expire
+	echo 550 > /sys/block/sdc/queue/iosched/sync_write_expire
+	echo 250 > /sys/block/sdc/queue/iosched/async_read_expire
+	echo 450 > /sys/block/sdc/queue/iosched/async_write_expire
+	echo 10 > /sys/block/sdc/queue/iosched/sleep_latency_multiple
+
+# Set I/O Scheduler tweaks sdd
+	chmod 644 /sys/block/sdd/queue/scheduler
+	echo maple > /sys/block/sdd/queue/scheduler
+	echo 512 > /sys/block/sdd/queue/read_ahead_kb
+	echo 4 > /sys/block/sdd/queue/iosched/writes_starved
+	echo 16 > /sys/block/sdd/queue/iosched/fifo_batch
+	echo 350 > /sys/block/sdd/queue/iosched/sync_read_expire
+	echo 550 > /sys/block/sdd/queue/iosched/sync_write_expire
+	echo 250 > /sys/block/sdd/queue/iosched/async_read_expire
+	echo 450 > /sys/block/sdd/queue/iosched/async_write_expire
+	echo 10 > /sys/block/sdd/queue/iosched/sleep_latency_multiple
+
+# Enable FSYNC
+	echo "N" > /sys/module/sync/parameters/fsync_enableds
+
+# Don't treat storage as rotational
+	echo 0 > /sys/block/mmcblk0/queue/rotational
+
+# Virtual Memory
+echo "1" > /proc/sys/vm/laptop_mode
 
 # Knox set to 0 on working system
 /sbin/resetprop -n ro.boot.warranty_bit "0"
@@ -38,6 +111,32 @@ $BB mount -o remount,rw /;
 /sbin/resetprop -n ro.boot.verifiedbootstate "green"
 /sbin/resetprop -n ro.boot.flash.locked "1"
 /sbin/resetprop -n ro.boot.ddrinfo "00000001"
+
+#-------------------------
+# MTWEAKS
+#-------------------------
+
+	# Make internal storage directory.
+    if [ ! -d $MTWEAKS_PATH ]; then
+	    $BB mkdir $MTWEAKS_PATH;
+    fi;
+
+	$BB chmod 0777 $MTWEAKS_PATH;
+	$BB chown 0.0 $MTWEAKS_PATH;
+
+	# Delete backup directory
+	$BB rm -rf $MTWEAKS_PATH/bk;
+
+    # Make backup directory.
+	$BB mkdir $MTWEAKS_PATH/bk;
+	$BB chmod 0777 $MTWEAKS_PATH/bk;
+	$BB chown 0.0 $MTWEAKS_PATH/bk;
+
+	# Save original voltages
+	$BB cat /sys/devices/system/cpu/cpufreq/mp-cpufreq/cluster1_volt_table > $MTWEAKS_PATH/bk/cpuCl1_stock_voltage
+	$BB cat /sys/devices/system/cpu/cpufreq/mp-cpufreq/cluster0_volt_table > $MTWEAKS_PATH/bk/cpuCl0_stock_voltage
+	$BB cat /sys/devices/14ac0000.mali/volt_table > $MTWEAKS_PATH/bk/gpu_stock_voltage
+	$BB chmod -R 755 $MTWEAKS_PATH/bk/*;
 
 # Deepsleep fix
 su -c 'echo "temporary none" >> /sys/class/scsi_disk/0:0:0:0/cache_type'
